0234dd2a1365bc95fcc413ab380de21c1aca9da3
MustafaShf <itsmustafashafique@gmail.com>
2024-03-25 20:07:21 +0500

    build:WeatherApp

---
+++ script.js
@@ -0,0 +1,26 @@
+// JavaScript code
+document.querySelector("button").addEventListener("click", function() {
+    let inputValue = document.querySelector("input").value; // Get the value from the input field
+    const apiKey = "7c98d0de87194fd9995111536242503";
+    const url = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${inputValue}`; // Use the input value in the API request
+
+    let weatherLoc = document.querySelector("h1");
+    let temp = document.querySelector("h2");
+
+    fetch(url, {
+        headers: {
+            Accept: "application/json" 
+        }
+    })
+    .then(response => {
+        return response.json();
+    })
+    .then(data => {
+        
+        weatherLoc.innerText = data.location.name;
+        temp.innerText = `${data.current.temp_c} °C`;
+    })
+    .catch(error => {
+        console.log(error);
+    });
+});
